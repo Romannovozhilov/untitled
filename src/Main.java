4499c42a686b048293ba39ddc4ac61195309ee55
Moogen <moongametv@gmail.com>
2018-03-24 00:22:08 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner("dfdfdfdfd");
+        System.out.println(scanner.delimiter());
+        System.out.println(scanner.locale());
+        System.out.println(scanner.radix());
+        System.out.println(scanner);
+    }
+}
